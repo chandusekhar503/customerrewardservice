6767c082fcd1d815b3b8459eb5ecc067a32d7eb6
chandusekhar503 <chandusekhar503@gmail.com>
2025-04-08 13:35:56 +0530

    spring boot project structure commit

---
+++ src/main/java/com/retailer/customerrewardservice/CustomerrewardserviceApplication.java
@@ -0,0 +1,13 @@
+package com.retailer.customerrewardservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerrewardserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerrewardserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/retailer/customerrewardservice/CustomerrewardserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.retailer.customerrewardservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerrewardserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
